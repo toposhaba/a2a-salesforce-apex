d985a4ce74e7e4f8062a7f03c83392118b4b5eb5
Kabir Khan <kkhan@redhat.com>
2025-05-07 11:24:50 +0100

    Use multi-line strings for JSON

--- src/test/java/io/a2a/client/A2AClientTest.java
+++ src/test/java/io/a2a/client/A2AClientTest.java
@@ -47,89 +47,91 @@ public class A2AClientTest {
         assertEquals("Google Maps Agent", client.getAgentCard().name());
     }
 
-    private static final String TASK_RESPONSE = "{\n" +
-            " \"jsonrpc\": \"2.0\",\n" +
-            " \"id\": 1,\n" +
-            " \"result\": {\n" +
-            "  \"id\": \"de38c76d-d54c-436c-8b9f-4c2703648d64\",\n" +
-            "  \"sessionId\": \"c295ea44-7543-4f78-b524-7a38915ad6e4\",\n" +
-            "  \"status\": {\n" +
-            "   \"state\": \"completed\"\n" +
-            "  },\n" +
-            "  \"artifacts\": [\n" +
-            "   {\n" +
-            "    \"name\": \"joke\",\n" +
-            "    \"parts\": [\n" +
-            "     {\n" +
-            "      \"type\": \"text\",\n" +
-            "      \"text\": \"Why did the chicken cross the road? To get to the other side!\"\n" +
-            "     }\n" +
-            "    ]\n" +
-            "   }\n" +
-            "  ],\n" +
-            "  \"metadata\": {}\n" +
-            " }\n" +
-            "}";
+    private static final String TASK_RESPONSE = """
+            {
+             "jsonrpc": "2.0",
+             "id": 1,
+             "result": {
+              "id": "de38c76d-d54c-436c-8b9f-4c2703648d64",
+              "sessionId": "c295ea44-7543-4f78-b524-7a38915ad6e4",
+              "status": {
+               "state": "completed"
+              },
+              "artifacts": [
+               {
+                "name": "joke",
+                "parts": [
+                 {
+                  "type": "text",
+                  "text": "Why did the chicken cross the road? To get to the other side!"
+                 }
+                ]
+               }
+              ],
+              "metadata": {}
+             }
+            }""";
 
-    private static final String AGENT_CARD = "{\n" +
-            "  \"name\": \"Google Maps Agent\",\n" +
-            "  \"description\": \"Plan routes, remember places, and generate directions\",\n" +
-            "  \"url\": \"https://maps-agent.google.com\",\n" +
-            "  \"provider\": {\n" +
-            "    \"organization\": \"Google\",\n" +
-            "    \"url\": \"https://google.com\"\n" +
-            "  },\n" +
-            "  \"version\": \"1.0.0\",\n" +
-            "  \"authentication\": {\n" +
-            "    \"schemes\": [\"OAuth2\"]\n" +
-            "  },\n" +
-            "  \"defaultInputModes\": [\n" +
-            "    \"text/plain\"\n" +
-            "  ],\n" +
-            "  \"defaultOutputModes\": [\n" +
-            "    \"text/plain\",\n" +
-            "    \"application/html\"\n" +
-            "  ],\n" +
-            "  \"capabilities\": {\n" +
-            "    \"streaming\": true,\n" +
-            "    \"pushNotifications\": false\n" +
-            "  },\n" +
-            "  \"skills\": [\n" +
-            "    {\n" +
-            "      \"id\": \"route-planner\",\n" +
-            "      \"name\": \"Route planning\",\n" +
-            "      \"description\": \"Helps plan routing between two locations\",\n" +
-            "      \"tags\": [\n" +
-            "        \"maps\",\n" +
-            "        \"routing\",\n" +
-            "        \"navigation\"\n" +
-            "      ],\n" +
-            "      \"examples\": [\n" +
-            "        \"plan my route from Sunnyvale to Mountain View\",\n" +
-            "        \"what's the commute time from Sunnyvale to San Francisco at 9AM\",\n" +
-            "        \"create turn by turn directions from Sunnyvale to Mountain View\"\n" +
-            "      ],\n" +
-            "      \"outputModes\": [\n" +
-            "        \"application/html\",\n" +
-            "        \"video/mp4\"\n" +
-            "      ]\n" +
-            "    },\n" +
-            "    {\n" +
-            "      \"id\": \"custom-map\",\n" +
-            "      \"name\": \"My Map\",\n" +
-            "      \"description\": \"Manage a custom map with your own saved places\",\n" +
-            "      \"tags\": [\n" +
-            "        \"custom-map\",\n" +
-            "        \"saved-places\"\n" +
-            "      ],\n" +
-            "      \"examples\": [\n" +
-            "        \"show me my favorite restaurants on the map\",\n" +
-            "        \"create a visual of all places I've visited in the past year\"\n" +
-            "      ],\n" +
-            "      \"outputModes\": [\n" +
-            "        \"application/html\"\n" +
-            "      ]\n" +
-            "    }\n" +
-            "  ]\n" +
-            "}";
+    private static final String AGENT_CARD = """
+            {
+              "name": "Google Maps Agent",
+              "description": "Plan routes, remember places, and generate directions",
+              "url": "https://maps-agent.google.com",
+              "provider": {
+                "organization": "Google",
+                "url": "https://google.com"
+              },
+              "version": "1.0.0",
+              "authentication": {
+                "schemes": ["OAuth2"]
+              },
+              "defaultInputModes": [
+                "text/plain"
+              ],
+              "defaultOutputModes": [
+                "text/plain",
+                "application/html"
+              ],
+              "capabilities": {
+                "streaming": true,
+                "pushNotifications": false
+              },
+              "skills": [
+                {
+                  "id": "route-planner",
+                  "name": "Route planning",
+                  "description": "Helps plan routing between two locations",
+                  "tags": [
+                    "maps",
+                    "routing",
+                    "navigation"
+                  ],
+                  "examples": [
+                    "plan my route from Sunnyvale to Mountain View",
+                    "what's the commute time from Sunnyvale to San Francisco at 9AM",
+                    "create turn by turn directions from Sunnyvale to Mountain View"
+                  ],
+                  "outputModes": [
+                    "application/html",
+                    "video/mp4"
+                  ]
+                },
+                {
+                  "id": "custom-map",
+                  "name": "My Map",
+                  "description": "Manage a custom map with your own saved places",
+                  "tags": [
+                    "custom-map",
+                    "saved-places"
+                  ],
+                  "examples": [
+                    "show me my favorite restaurants on the map",
+                    "create a visual of all places I've visited in the past year"
+                  ],
+                  "outputModes": [
+                    "application/html"
+                  ]
+                }
+              ]
+            }""";
 }
