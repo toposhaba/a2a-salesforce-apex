dcac0df41b2219f2cc1ea15a3494b86e58bc5866
Kabir Khan <kkhan@redhat.com>
2025-05-19 11:48:35 +0200

    Skeleton request handlers

--- src/main/java/io/a2a/server/requesthandlers/RequestHandler.java
+++ src/main/java/io/a2a/server/requesthandlers/RequestHandler.java
@@ -8,26 +8,30 @@ import io.a2a.spec.GetTaskPushNotificationRequest;
 import io.a2a.spec.GetTaskPushNotificationResponse;
 import io.a2a.spec.GetTaskRequest;
 import io.a2a.spec.GetTaskResponse;
+import io.a2a.spec.MessageSendParams;
 import io.a2a.spec.SendMessageRequest;
 import io.a2a.spec.SendMessageResponse;
 import io.a2a.spec.SendStreamingMessageRequest;
 import io.a2a.spec.SendStreamingMessageResponse;
 import io.a2a.spec.SetTaskPushNotificationRequest;
 import io.a2a.spec.SetTaskPushNotificationResponse;
+import io.a2a.spec.TaskIdParams;
+import io.a2a.spec.TaskPushNotificationConfig;
+import io.a2a.spec.TaskQueryParams;
 import io.a2a.spec.TaskResubscriptionRequest;
 
 public interface RequestHandler {
-    GetTaskResponse onGetTask(GetTaskRequest request) throws JSONRPCException;
+    GetTaskResponse onGetTask(TaskQueryParams params) throws JSONRPCException;
 
-    CancelTaskResponse onCancelTask(GetTaskRequest request) throws JSONRPCException;
+    CancelTaskResponse onCancelTask(TaskIdParams params) throws JSONRPCException;
 
-     SendMessageResponse onMessageSend(SendMessageRequest request) throws JSONRPCException;
+     SendMessageResponse onMessageSend(MessageSendParams params) throws JSONRPCException;
 
-    SendStreamingMessageResponse onMessageSendStream(SendStreamingMessageRequest request) throws JSONRPCException;
+    SendStreamingMessageResponse onMessageSendStream(MessageSendParams params) throws JSONRPCException;
 
-    SetTaskPushNotificationResponse onSetTaskPushNotificationConfig(SetTaskPushNotificationRequest request) throws JSONRPCException;
+    SetTaskPushNotificationResponse onSetTaskPushNotificationConfig(TaskPushNotificationConfig params) throws JSONRPCException;
 
-    GetTaskPushNotificationResponse onGetTaskPushNotificationConfig(GetTaskPushNotificationRequest request) throws JSONRPCException;
+    GetTaskPushNotificationResponse onGetTaskPushNotificationConfig(TaskIdParams params) throws JSONRPCException;
 
-    Flow.Publisher<SendStreamingMessageResponse> onResubscribeToTask(TaskResubscriptionRequest request) throws JSONRPCException;
+    Flow.Publisher<SendStreamingMessageResponse> onResubscribeToTask(TaskIdParams params) throws JSONRPCException;
 }
