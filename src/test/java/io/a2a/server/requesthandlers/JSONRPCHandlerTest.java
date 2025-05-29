df25c53c4b059ebeb30a6a0260266bb2a6d230eb
Kabir Khan <kkhan@redhat.com>
2025-05-29 17:30:34 +0200

    Add testOnResubscribeNoExistingTaskError

--- src/test/java/io/a2a/server/requesthandlers/JSONRPCHandlerTest.java
+++ src/test/java/io/a2a/server/requesthandlers/JSONRPCHandlerTest.java
@@ -766,10 +766,46 @@ public class JSONRPCHandlerTest {
         assertEquals(events, results);
     }
 
-    @Disabled
     @Test
     public void testOnResubscribeNoExistingTaskError() {
-        
+        JSONRPCHandler handler = new JSONRPCHandler(CARD, requestHandler);
+
+        TaskResubscriptionRequest request = new TaskResubscriptionRequest("1", new TaskIdParams(MINIMAL_TASK.getId()));
+        Flow.Publisher<SendStreamingMessageResponse> response = handler.onResubscribeToTask(request);
+
+        List<SendStreamingMessageResponse> results = new ArrayList<>();
+        AtomicReference<Throwable> error = new AtomicReference<>();
+
+        response.subscribe(new Flow.Subscriber<SendStreamingMessageResponse>() {
+            private Flow.Subscription subscription;
+
+            @Override
+            public void onSubscribe(Flow.Subscription subscription) {
+                this.subscription = subscription;
+                subscription.request(1);
+            }
+
+            @Override
+            public void onNext(SendStreamingMessageResponse item) {
+                results.add(item);
+                subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                error.set(throwable);
+                subscription.cancel();
+            }
+
+            @Override
+            public void onComplete() {
+                subscription.cancel();
+            }
+        });
+
+        assertEquals(1, results.size());
+        assertNull(results.get(0).getResult());
+        assertInstanceOf(TaskNotFoundError.class, results.get(0).getError());
     }
 
     private static AgentCard createAgentCard(boolean streaming, boolean pushNotifications, boolean stateTransitionHistory) {
