ff8ee2565cb351646faf55dfbba7188ea3923488
Farah Juma <fjuma@redhat.com>
2025-06-03 17:14:48 -0400

    Ensure the timestamp for the TaskStatus can be serialized/deserialized correctly

--- src/main/java/io/a2a/spec/TaskStatus.java
+++ src/main/java/io/a2a/spec/TaskStatus.java
@@ -2,8 +2,10 @@ package io.a2a.spec;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+
 import io.a2a.util.Assert;
 
 /**
@@ -11,7 +13,8 @@ import io.a2a.util.Assert;
  */
 @JsonInclude(JsonInclude.Include.NON_ABSENT)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record TaskStatus(TaskState state, Message message, LocalDateTime timestamp) {
+public record TaskStatus(TaskState state, Message message,
+                         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS") LocalDateTime timestamp) {
 
     public TaskStatus {
         Assert.checkNotNullParam("state", state);
