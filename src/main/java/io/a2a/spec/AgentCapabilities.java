5c724533a4dfeb132ae3d31d4f7bada27dbf54fb
Farah Juma <fjuma@redhat.com>
2025-06-17 11:39:33 -0400

    Add support for extensions

--- src/main/java/io/a2a/spec/AgentCapabilities.java
+++ src/main/java/io/a2a/spec/AgentCapabilities.java
@@ -1,7 +1,42 @@
 package io.a2a.spec;
 
+import java.util.List;
+
 /**
  * An agent's capabilities.
  */
-public record AgentCapabilities(boolean streaming, boolean pushNotifications, boolean stateTransitionHistory) {
+public record AgentCapabilities(boolean streaming, boolean pushNotifications, boolean stateTransitionHistory,
+                                List<AgentExtension> extensions) {
+
+    public static class Builder {
+
+        private boolean streaming;
+        private boolean pushNotifications;
+        private boolean stateTransitionHistory;
+        private List<AgentExtension> extensions;
+
+        public Builder streaming(boolean streaming) {
+            this.streaming = streaming;
+            return this;
+        }
+
+        public Builder pushNotifications(boolean pushNotifications) {
+            this.pushNotifications = pushNotifications;
+            return this;
+        }
+
+        public Builder stateTransitionHistory(boolean stateTransitionHistory) {
+            this.stateTransitionHistory = stateTransitionHistory;
+            return this;
+        }
+
+        public Builder extensions(List<AgentExtension> extensions) {
+            this.extensions = extensions;
+            return this;
+        }
+
+        public AgentCapabilities build() {
+            return new AgentCapabilities(streaming, pushNotifications, stateTransitionHistory, extensions);
+        }
+    }
 }
